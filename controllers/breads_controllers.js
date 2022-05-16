52b8cf905a74bbcb81fb58a694c3fb341ecf64d2
Erick Falconi <raidenjr28@gmail.com>
2022-05-16 13:22:49 -0400

    Helper-Methods-Part2

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -34,7 +34,7 @@ breads.get('/new', (req, res) => {
 })
 
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
+breads.put('/:id', (req, res) => {
   if (req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
@@ -43,27 +43,58 @@ breads.put('/:arrayIndex', (req, res) => {
   Bread.findByIdAndUpdate(req.params.id, req.params.body, { new: true })
     .then(updatedBread => {
       console.log(updatedBread)
-      res.redirect(`/breads/${req.params.arrayIndex}`)
+      res.redirect(`/breads/${req.params.id}`)
     })
 
 })
 
 //EDIT
-breads.get('/:index.Array/edit', (req, res) => {
-  res.render('edit', {
-    bread: Bread[req.params.indexArray],
-    index: req.params.indexArray
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id)
+    .then(foundBread => {
+      res.render('edit', {
+        bread: foundBread
+      })
+    })
   })
-})
 
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
+breads.delete('/:id', (req, res) => {
   Bread.findByIdAndDelete(req.params.id)
     .then(deletedBread => {
       res.status(303).redirect('/breads')
     })
 
 })
+
+breads.get('/data/seed', (req, res) => {
+  Bread.insertMany([
+    {
+      name: 'Rye',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      name: 'French',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    },
+    {
+      name: 'Gluten Free',
+      hasGluten: false,
+      image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      name: 'Pumpernickel',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    }
+  ])
+    .then(createdBreads => {
+      res.redirect('/breads')
+    })
+})
+
 // SHOW
 breads.get('/:id', (req, res) => {
   Bread.findById(req.params.id)
